c2aae5eafb3692493a94788232abc0198f6f3132
Ömer Faruk ARTAN <o.farukartan@hotmail.com>
2021-07-31 20:58:49 +0300

    Initial commit

---
+++ src/Test.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        int math,physics,chemistry,Turkish,history,music ;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Matematik notunuzu giriniz :");
+        math = input.nextInt();
+
+        System.out.println("Fizik notunuzu giriniz :");
+        physics = input.nextInt();
+
+        System.out.println("Kimya notunuzu giriniz :");
+        chemistry = input.nextInt();
+
+        System.out.println("Türkçe notunuzu giriniz :");
+        Turkish = input.nextInt();
+
+        System.out.println("Tarih notunuzu giriniz :");
+        history = input.nextInt();
+
+        System.out.println("Müzik notunuzu giriniz :");
+        music = input.nextInt();
+
+        int sum = (math+physics+chemistry+Turkish+history+music);
+        double result = sum / 6.0 ;
+        String s = result > 60 ? "Sınıfı Geçti" : "Sınıfta Kaldı";
+        System.out.println(s);
+    }
+}
+
