749d344bfd1e7ce26fb82f7214191a9f57784a8d
merfrk <87045574+merfrk@users.noreply.github.com>
2021-08-20 12:38:54 +0300

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Fighter marc = new Fighter("Khabib" , 20 , 85, 90, 35);
-        Fighter alex = new Fighter("Alex" , 15 , 95, 100, 25);
-        Match r = new Match(marc,alex , 90 , 100);
-        r.run();
-
-    }
-}
